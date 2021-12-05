5e203602d8ee422c933384b889bf58b01fa2182a
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-05 10:12:10 +0100

    adds hover effect

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 export const StyledButton = styled.button`
   background-color: ${({variant}) => variant==='outline' ? '#FFF' : '#4caf50'};
   border: 2px solid #4caf50;
-  color: ${({variant}) => variant==='outline' ? '#4caf50' : '#FFF'};;
+  color: ${({variant}) => variant==='outline' ? '#4caf50' : '#FFF'};
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+
+  &:hover{
+  background-color: ${({variant}) => variant!=='outline' ? '#FFF' : '#4caf50'};
+  color: ${({variant}) => variant!=='outline' ? '#4caf50' : '#FFF'};
+  }
 `
 
 export const FancyButton = styled(StyledButton)`
